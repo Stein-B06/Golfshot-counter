0259c61261daf6a8891762fdf9a7cd7c2a3c9145
Stein-B06 <Steinb06.kodehode@gmail.com>
2023-10-24 18:15:55 +0200

    updated JS

--- index.js
+++ index.js
@@ -2,59 +2,69 @@ let holeNumber = 1;
 let currentNumber = 0;
 let incrementCount = 0;
 let incrementValueDisplay = document.getElementById("increment-value");
-let holeInfo = document.getElementById("hole-info");
 let incrementCountDisplay = document.getElementById("increment-count");
 let totalShotsDisplay = document.getElementById("total-shots");
+let holeInfoDisplay = document.getElementById("hole-info");
 
-// Load data from localStorage
-let savedHoleInfo = localStorage.getItem("holeInfo");
-if (savedHoleInfo) {
-  holeInfo.textContent = savedHoleInfo;
-}
 // Load the increment count and total shots from localStorage
+let savedIncrementValue = parseInt(localStorage.getItem("incrementValue")) || 0;
 let savedIncrementCount = parseInt(localStorage.getItem("incrementCount")) || 0;
 let savedTotalShots = parseInt(localStorage.getItem("totalShots")) || 0;
 
+// Set the initial increment value
+currentNumber = savedIncrementValue;
+incrementValueDisplay.textContent = `Increment Value: ${currentNumber}`;
 incrementCount = savedIncrementCount;
-totalShotsDisplay.textContent = `Total Shots: ${savedTotalShots}`;
 incrementCountDisplay.textContent = `Increment Count: ${incrementCount}`;
+totalShotsDisplay.textContent = `Total Shots: ${savedTotalShots}`;
+
+// Load hole info from localStorage
+let savedHoleInfo = localStorage.getItem("holeInfo") || "";
+if (savedHoleInfo) {
+  holeInfoDisplay.textContent = savedHoleInfo;
+}
 
 function increment() {
-  // Increment the number by 1
   currentNumber += 1;
-  incrementValueDisplay.textContent = `Golfshots: ${currentNumber}`;
+  incrementValueDisplay.textContent = `Increment Value: ${currentNumber}`;
   incrementCount++;
   incrementCountDisplay.textContent = `Increment Count: ${incrementCount}`;
+
+  // Store the increment value in local storage
+  localStorage.setItem("incrementValue", currentNumber);
+  localStorage.setItem("incrementCount", incrementCount);
 }
 
 function save() {
-  // Get the current number and display it along with the current hole number
-  holeInfo.textContent += `(Hole ${holeNumber}) ${currentNumber} - `;
+  holeInfoDisplay.textContent += `(Hole ${holeNumber}) ${currentNumber} - `;
   holeNumber++;
-  // Reset the current number to 0
   currentNumber = 0;
-  incrementValueDisplay.textContent = `Golfshots: 0`;
+  incrementValueDisplay.textContent = `Increment Value: 0`;
 
-  // Store data in localStorage
-  localStorage.setItem("holeInfo", holeInfo.textContent);
+  // Store hole info, increment count, incrementvalue and total shots in local storage with unique keys
+  localStorage.setItem("holeInfo", holeInfoDisplay.textContent);
+  localStorage.setItem("incrementCount", incrementCount);
+  localStorage.setItem("totalShots", incrementCount);
+  localStorage.setItem("incrementValue", currentNumber);
 }
 
 function reset() {
-  // Clear data from localStorage
-  localStorage.removeItem("holeInfo");
+  // Clear data from localStorage and reset displays
+  localStorage.removeItem("incrementValue");
   localStorage.removeItem("incrementCount");
   localStorage.removeItem("totalShots");
-  holeInfo.textContent = "";
+  localStorage.removeItem("holeInfo");
+  holeInfoDisplay.textContent = "";
   holeNumber = 1;
   currentNumber = 0;
-  incrementValueDisplay.textContent = `Golfshots: 0`;
+  incrementValueDisplay.textContent = `Increment Value: 0`;
   incrementCount = 0;
   incrementCountDisplay.textContent = `Increment Count: 0`;
-  totalShotsDisplay.textContent = ""; // Clear total shots display
+  totalShotsDisplay.textContent = `Total Shots: 0`;
 }
+
 function displayTotalShots() {
   // Display the total number of shots and store it in localStorage
   totalShotsDisplay.textContent = `Total Shots: ${incrementCount}`;
   localStorage.setItem("totalShots", incrementCount);
-  localStorage.setItem("incrementCount", incrementCount);
 }
