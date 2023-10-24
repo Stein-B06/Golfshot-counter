fa88223c5cfc8cb3959a1bca0a1e024279cba233
Stein-B06 <Steinb06.kodehode@gmail.com>
2023-10-24 14:28:26 +0200

    Added JS, CSS and HTML

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 let holeNumber = 1;
-let holeInfo = document.getElementById("hole-info");
 let currentNumber = 0;
+let incrementCount = 0;
 let incrementValueDisplay = document.getElementById("increment-value");
+let holeInfo = document.getElementById("hole-info");
+let incrementCountDisplay = document.getElementById("increment-count");
+let totalShotsDisplay = document.getElementById("total-shots");
+
+// Load data from localStorage
+let savedHoleInfo = localStorage.getItem("holeInfo");
+if (savedHoleInfo) {
+  holeInfo.textContent = savedHoleInfo;
+}
+// Load the increment count and total shots from localStorage
+let savedIncrementCount = parseInt(localStorage.getItem("incrementCount")) || 0;
+let savedTotalShots = parseInt(localStorage.getItem("totalShots")) || 0;
+
+incrementCount = savedIncrementCount;
+totalShotsDisplay.textContent = `Total Shots: ${savedTotalShots}`;
+incrementCountDisplay.textContent = `Increment Count: ${incrementCount}`;
 
 function increment() {
   // Increment the number by 1
   currentNumber += 1;
   incrementValueDisplay.textContent = `Golfshots: ${currentNumber}`;
+  incrementCount++;
+  incrementCountDisplay.textContent = `Increment Count: ${incrementCount}`;
 }
 
 function save() {
@@ -16,12 +34,27 @@ function save() {
   // Reset the current number to 0
   currentNumber = 0;
   incrementValueDisplay.textContent = `Golfshots: 0`;
+
+  // Store data in localStorage
+  localStorage.setItem("holeInfo", holeInfo.textContent);
 }
 
 function reset() {
-  // Clear the hole information, reset the hole number, and reset the increment value
+  // Clear data from localStorage
+  localStorage.removeItem("holeInfo");
+  localStorage.removeItem("incrementCount");
+  localStorage.removeItem("totalShots");
   holeInfo.textContent = "";
   holeNumber = 1;
   currentNumber = 0;
   incrementValueDisplay.textContent = `Golfshots: 0`;
+  incrementCount = 0;
+  incrementCountDisplay.textContent = `Increment Count: 0`;
+  totalShotsDisplay.textContent = ""; // Clear total shots display
+}
+function displayTotalShots() {
+  // Display the total number of shots and store it in localStorage
+  totalShotsDisplay.textContent = `Total Shots: ${incrementCount}`;
+  localStorage.setItem("totalShots", incrementCount);
+  localStorage.setItem("incrementCount", incrementCount);
 }
